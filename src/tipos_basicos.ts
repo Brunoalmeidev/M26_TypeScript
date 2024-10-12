acf2b3e11417c324ac970dd03c8a7c179992d831
Bruno Almeida <brunoalmeida2846@hotmail.com>
2024-10-11 22:03:02 -0300

    first commit

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,31 @@
+// boolean
+let estaChovendo: boolean = false
+
+estaChovendo = true
+
+// number
+let idade: number = 27
+
+let altura: number = 1.75
+
+// string
+const nacionalidade: string = 'brasileira'
+
+// array
+const colegas: string[] = ['lucas', 'fernanda']
+const tecnologias: Array<string> = ['HTML', 'CSS', 'JS']
+const notas: ReadonlyArray<number> = [7, 9, 5, 8];
+
+// tupla: Tuplas podem ser usadas para armazenar conjuntos de valores de tipos distintos
+const lista: [nome: string, estaEstudando: boolean, idade: number] = ['bruno', true, 27]
+
+// union
+let idadeDaAna: number | string = 25
+idadeDaAna = '25 anos'
+
+// any: any aceita qualquer tipo de dado
+let dadosDaApi: any;
+dadosDaApi = 10
+dadosDaApi = true
+dadosDaApi = [1,2,3]
+dadosDaApi = 'string'
