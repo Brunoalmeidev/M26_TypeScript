5a837af8d888dbf7edfd42baf74a1284b906fda0
Bruno Almeida <brunoalmeida2846@hotmail.com>
2024-10-16 19:02:12 -0300

    explicando o código

--- src/tipos_customizados.js
+++ src/tipos_customizados.js
@@ -1,4 +1,6 @@
 "use strict";
+// Criando um array de objetos do tipo 'aluno', chamado 'alunos'
+// Cada objeto segue a estrutura definida no tipo 'aluno'
 const alunos = [
     {
         nome: "Bruno",
@@ -11,15 +13,22 @@ const alunos = [
         idade: 23,
     },
 ];
+// Adicionando um novo aluno ao array 'alunos' usando o método 'push'
+// O novo aluno deve seguir a estrutura do 'aluno'
 alunos.push({
     nome: "Julia",
     cursos: ["Arquitetura"],
     idade: 29
 });
+// Criando um novo objeto do tipo 'aluno', chamado 'novoAluno'
+// Observe que a propriedade 'cursos' não está presente, já que ela é opcional
 const novoAluno = {
     nome: "Lucas",
     idade: 32,
 };
+// Função para exibir o nome de um aluno
+// A função recebe um objeto do tipo 'aluno' como parâmetro e imprime o nome desse aluno no console
 function exibeAluno(aluno) {
     console.log(aluno.nome);
 }
+;
--- src/tipos_customizados.ts
+++ src/tipos_customizados.ts
@@ -1,9 +1,16 @@
+// Definindo um tipo chamado 'aluno', que vai estruturar os objetos de aluno
+// O tipo 'aluno' contém três propriedades: 
+// - 'nome', uma string que é obrigatória
+// - 'cursos', um array de strings que é opcional (representado pelo '?')
+// - 'idade', um número que é obrigatório
 type aluno = {
     nome: string;
     cursos?: string[];
     idade: number;
-}
+};
 
+// Criando um array de objetos do tipo 'aluno', chamado 'alunos'
+// Cada objeto segue a estrutura definida no tipo 'aluno'
 const alunos: aluno[] = [
     {
         nome: "Bruno",
@@ -15,19 +22,29 @@ const alunos: aluno[] = [
         cursos: ["Front-end", "Python"],
         idade: 23,
     },
-]
+];
 
+
+// Adicionando um novo aluno ao array 'alunos' usando o método 'push'
+// O novo aluno deve seguir a estrutura do 'aluno'
 alunos.push({
     nome: "Julia",
     cursos: ["Arquitetura"],
     idade: 29
-})
+});
 
+
+// Criando um novo objeto do tipo 'aluno', chamado 'novoAluno'
+// Observe que a propriedade 'cursos' não está presente, já que ela é opcional
 const novoAluno: aluno = {
     nome: "Lucas",
     idade: 32,
-}
+};
 
+
+// Função para exibir o nome de um aluno
+// A função recebe um objeto do tipo 'aluno' como parâmetro e imprime o nome desse aluno no console
 function exibeAluno(aluno: aluno) {
     console.log(aluno.nome)
-}
+};
+
