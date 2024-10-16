683e3c4a250a2d72f974ee75f548563e1ecbecf1
Bruno Almeida <brunoalmeida2846@hotmail.com>
2024-10-16 16:33:21 -0300

    crie casting

---
+++ src/tipos_basicos.js
@@ -0,0 +1,26 @@
+"use strict";
+// boolean
+let estaChovendo = false;
+estaChovendo = true;
+// number
+let idade = 27;
+let altura = 1.75;
+// string
+const nacionalidade = 'brasileira';
+// array
+const colegas = ['lucas', 'fernanda'];
+const tecnologias = ['HTML', 'CSS', 'JS'];
+const notas = [7, 9, 5, 8];
+// tupla: Tuplas podem ser usadas para armazenar conjuntos de valores de tipos distintos
+const lista = ['bruno', true, 27];
+// union
+let idadeDaAna = 25;
+idadeDaAna = '25 anos';
+// any: any aceita qualquer tipo de dado
+let dadosDaApi;
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = 'string';
+// 
+let curso = 'front-end';
